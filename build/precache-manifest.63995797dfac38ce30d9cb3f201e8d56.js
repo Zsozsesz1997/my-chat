e9a2b6ae58e227dcfaf4f3997c64e9b4e8bd1e71
cziko_zsolt_v8ue4a <zsozsesz199712@gmail.com>
2020-04-16 22:49:33 +0200

    release

---
+++ build/precache-manifest.63995797dfac38ce30d9cb3f201e8d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0f800fe6dc2f8ecc4b626e3f4525d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd9b99e8b80b3d19eff6",
+    "url": "./static/css/main.181f8504.chunk.css"
+  },
+  {
+    "revision": "1f1012de07112484eafe",
+    "url": "./static/js/2.e8b63911.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e8b63911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9b99e8b80b3d19eff6",
+    "url": "./static/js/main.335b9531.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
